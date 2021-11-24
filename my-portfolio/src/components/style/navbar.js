c1e1fd85430b7c32ae90be62b2999330e7c1524d
Cole Cochran <colecochran405@gmail.com>
2021-11-24 14:29:09 -0600

    creating navbar.js

---
+++ my-portfolio/src/components/style/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../assets/css/style.css'
+
+function NavBar() {
+    return (
+<nav class="navbar">
+<ul>
+    <a href="#about-me">About</a>
+    <a href="#work">Work</a>
+    <a href="#contact-me">Contact Me</a>
+    <a href="https://drive.google.com/file/d/146tspX44vi961bY4gLJK_JHb0BcaLX1h/view?usp=sharing">Resume</a>
+</ul>
+</nav>
+)
+}
+export default NavBar;
