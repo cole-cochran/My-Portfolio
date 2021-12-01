62c527b78e322bc86977f7a240f06ebfd1b46f1d
Cole Cochran <colecochran405@gmail.com>
2021-11-30 19:26:21 -0600

    resolving the mui card bodies for portfolio.

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import Body from './components/style/body';
+import Body from './components/style/Body';
+import Work from './components/pages/Work'
+import NavBar from './components/style/Navbar';
+import Footer from './components/style/Footer'
 
 function App() {
-  return <Body />;
+  return 
+    <Work />;
 }
 
 export default App;
---
+++ my-portfolio/src/components/pages/Work.js
@@ -0,0 +1,188 @@
+import  React from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+
+export default function Cards() {
+ 
+    const theme = createTheme();
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <main>
+
+
+
+          <Container sx={{ py: 8 }} maxWidth="lg">
+             End hero unit
+            <Grid container spacing={4}>
+            <Grid item  xs={12} sm={6} md={4}>
+            <Card  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+      <CardMedia
+        component="img"
+        //height="140"
+        sx={{
+            16:9
+            // pt: '56.25%',
+          }}
+          image="./images/imgs/plannerpic.avif"        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Event-Planner
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Link To Github</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    </Grid>
+    <Grid item  xs={12} sm={6} md={4}>
+
+    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="./images/imgs/projectOne.avif"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Project-One
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    </Grid>
+    <Grid item  xs={12} sm={6} md={4}>
+
+    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="./images/imgs/weatherpic.avif"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Weather Dashboard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+    </Grid>
+    <Grid item  xs={12} sm={6} md={4}>
+
+    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="./images/imgs/h2.avif"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Feed Your Lifestyle
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  
+
+</Grid>
+
+<Grid item  xs={12} sm={6} md={4}>
+
+    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="#"
+        alt="Coming Soon"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Feed Your Lifestyle
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  
+
+</Grid>
+<Grid item  xs={12} sm={6} md={4}>
+
+    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="./images/imgs/h1.avif"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Budgeting App
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  
+
+</Grid>
+</Grid>
+</Container>
+
+
+</main>
+      </ThemeProvider>
+    );
+  }
