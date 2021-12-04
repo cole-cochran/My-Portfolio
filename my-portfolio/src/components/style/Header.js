1341002c0f32e6525aad08461d6452a5b609ed98
Cole Cochran <colecochran405@gmail.com>
2021-12-04 15:09:29 -0600

    fixing header

--- my-portfolio/src/components/style/Header.js
+++ my-portfolio/src/components/style/Header.js
@@ -45,7 +45,7 @@ const ResponsiveAppBar = () => {
             component="div"
             sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
           >
-            LOGO
+            Cole Cochran
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
