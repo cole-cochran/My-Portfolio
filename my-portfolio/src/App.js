a28d0a5a9579ca84f396666afaa64960651d52cf
Cole Cochran <colecochran405@gmail.com>
2021-12-07 13:19:49 -0600

    fixing submit form

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -15,7 +15,6 @@ function App() {
     <Body />
     <Work />
     <Contact />
-    <Footer />
     </div>
 
   );
--- my-portfolio/src/components/style/Contact.js
+++ my-portfolio/src/components/style/Contact.js
@@ -1,5 +1,6 @@
-import { Col, Row, Form, InputGroup, Button } from "react-bootstrap";
+import { Col, Row, Form, InputGroup, Button, FormControl } from "react-bootstrap";
 import React, { useState } from 'react';
+import { Typography } from "@mui/material";
 // import ReactDOM from 'react-dom';
 
   
@@ -16,58 +17,56 @@ function Contact() {
     };
   
     return (
-      <Form noValidate validated={validated} onSubmit={handleSubmit}>
-        <Row className="mb-3">
-          <Form.Group as={Col} md="4" controlId="validationCustom01">
-            <Form.Label>First name</Form.Label>
+      <Form noValidate validated={validated} onSubmit={handleSubmit} className="signup-form-section">
+          <Typography>
+              Want to work together? Send me an email!
+           </Typography>   
+          <Form.Group as={Col} md="2" controlId="validationCustom01">
+            <Form.Label>Name</Form.Label>
             <Form.Control
               required
               type="text"
-              placeholder="First name"
-        
+
             />
             <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
           </Form.Group>
-          <Form.Group as={Col} md="4" controlId="validationCustom02">
-            <Form.Label>Last name</Form.Label>
-            <Form.Control
-              required
-              type="text"
-              placeholder="Last name"
-     
-            />
-            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group as={Col} md="4" controlId="validationCustomEmail">
+          
+          <Form.Group as={Col} md="2" controlId="validationCustom02">
             <Form.Label>Email</Form.Label>
-            <InputGroup hasValidation>
-              <InputGroup.Text id="inputGroupPrepend"></InputGroup.Text>
-              <Form.Control
-                type="text"
-                placeholder="Email"
-                aria-describedby="inputGroupPrepend"
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-                Please enter a valid email.
-              </Form.Control.Feedback>
-            </InputGroup>
+            <Form.Control
+              required
+              type="text"
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
           </Form.Group>
-        </Row>
-        <Row className="mb-3">
-          <Form.Group as={Col} md="6" controlId="validationCustom03">
+          <Form.Group as={Col} md="2" controlId="validationCustom03">
             <Form.Label>Message</Form.Label>
-            <Form.Control type="text" placeholder="Message" required />
+            <Form.Control type="text" required />
             <Form.Control.Feedback type="invalid">
               Please provide a valid message.
             </Form.Control.Feedback>
           </Form.Group>
-        </Row>
-        <Form.Group className="mb-3">
+          <Form.Group className="mb-3">
         </Form.Group>
         <Button type="submit">Submit form</Button>
       </Form>
+      
     );
   }
-  
+<Form>
+        <Form.Group as={Col} md="2" controlId="validationCustom01">
+           <Form.Label>Email address</Form.Label>
+           <Form.Control type="email" placeholder="Enter email" />
+           <Form.Text className="text-muted"> We'll never share your email with anyone else. </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+           <Form.Label>Password</Form.Label>
+           <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+           <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group> <Button variant="primary" type="submit"> Submit </Button>
+     </Form>
+
+
   export default Contact;
